ea3828eb6daf63f2eebf843e77cdbe65f67bd88f
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-18 16:34:41 -0700

    add tests to check if you were eaten and use a feeding function

--- __tests__/hungrybear.test.js
+++ __tests__/hungrybear.test.js
@@ -22,4 +22,20 @@ describe("Fuzzy", () => {
     jest.advanceTimersByTime(3001);
     expect(fuzzy.foodLevel).toEqual(7);
   });
+
+  test("should get very hungry if the food level is below 0", function() {
+    fuzzy.foodLevel = 0;
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  test("should get very hungry if 10 seconds pass without feeding", function() {
+    jest.advanceTimersByTime(10001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  test("should have a food level of 10 if it is fed", function() {
+    jest.advanceTimersByTime(9001);
+    fuzzy.feed();
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
 });
